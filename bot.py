6a2f6128b40d3337b9f96ad98e819e91ce5ca767
leanseller <danil.d776@gmail.com>
2023-11-10 13:56:44 +0600

    initial commit

---
+++ bot.py
@@ -0,0 +1,42 @@
+from aiogram import Bot, Dispatcher
+
+import logging
+import asyncio
+
+from creds import BOT_TOKEN
+
+from handlers import users_handlers
+
+
+bot: Bot = Bot(BOT_TOKEN, parse_mode='html')
+dp: Dispatcher = Dispatcher()
+
+
+# Инициализируем логгер
+logger: logging = logging.getLogger(__name__)
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
+               u'[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting Bot')
+
+    # Регистрируем обработчики
+    dp.include_router(users_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+# Запускаем бота
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error('Bot Stopped!')
---
+++ directory/action_buttons.py
@@ -0,0 +1,9 @@
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import KeyboardButton
+
+
+start_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+
+start_buttons_list = [
+    ""
+]
