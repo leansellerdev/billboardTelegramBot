344abf0b62530d820957b31f6d575eda7da29b7d
ddudk <danil.d776@gmail.com>
2023-11-15 17:01:40 +0600

    main menu handlers were added

---
+++ core/handlers/start_handlers.py
@@ -0,0 +1,65 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters.command import CommandStart
+from aiogram.fsm.context import FSMContext
+
+from core.states.states import FSMStart
+from core.buttons.action_buttons import (not_registered_kb_builder,
+                                         registered_kb_builder)
+
+from core.utils.utils import user_registered
+
+router: Router = Router()
+# user_registered = False
+
+
+@router.message(CommandStart())
+@router.message(F.text == "Отмена")
+async def start(message: Message, state: FSMContext):
+    username = message.from_user.first_name
+    await state.set_state(FSMStart.start)
+
+    if not await user_registered(message.from_user.id):
+        await message.answer(
+            text=f'Здравствуйте, {username}!\nВыберите действие:',
+            reply_markup=not_registered_kb_builder.as_markup(
+                resize_keyboard=True
+            )
+        )
+    else:
+        await message.answer(
+            text=f'Здравствуйте, {username}!\nВыберите действие:',
+            reply_markup=registered_kb_builder.as_markup(
+                resize_keyboard=True
+            )
+        )
+
+
+@router.message(F.text == "Мои заказы", FSMStart.start)
+async def self_orders(message: Message, state: FSMContext):
+
+    # await state.set_state(FSMStart.self_orders)
+
+    await message.answer(
+        text="Coming Soon..."
+    )
+
+
+@router.message(F.text == "Биллборды", FSMStart.start)
+async def billboards(message: Message, state: FSMContext):
+
+    # await state.set_state(FSMStart.billboards)
+
+    await message.answer(
+        text="Coming Soon..."
+    )
+
+
+@router.message(F.text == "О нас", FSMStart.start)
+async def about_us(message: Message, state: FSMContext):
+
+    # await state.set_state(FSMStart.about)
+
+    await message.answer(
+        text="Coming Soon..."
+    )
