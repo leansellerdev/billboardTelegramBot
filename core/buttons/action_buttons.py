e939918cdb616d89a9393bef55dcc17ba6c90341
ddudk <danil.d776@gmail.com>
2023-11-15 16:58:03 +0600

    cancel button added

--- core/buttons/action_buttons.py
+++ core/buttons/action_buttons.py
@@ -5,6 +5,8 @@ from aiogram.types import KeyboardButton
 registered_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 not_registered_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 
+cancel_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+
 start_buttons_registered = [
     "Мои заказы", "Биллборды", "О нас"
 ]
@@ -13,8 +15,12 @@ start_buttons_not_registered = [
     "Регистрация", "Биллборды", "О нас"
 ]
 
+cancel_button = KeyboardButton(text="Отмена")
+
 registered_buttons: list[KeyboardButton] = [KeyboardButton(text=value) for value in start_buttons_registered]
 not_registered_buttons: list[KeyboardButton] = [KeyboardButton(text=value) for value in start_buttons_not_registered]
 
 registered_kb_builder.row(*registered_buttons, width=3)
 not_registered_kb_builder.row(*not_registered_buttons, width=3)
+
+cancel_kb_builder.add(cancel_button)
